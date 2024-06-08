ce2cfa0fb096544a4edd15c28947555af1848cf5
finiral <finiral@yahoo.fr>
2024-06-08 11:17:18 +0300

    testé keli

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,14 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->model("Reservation_Model");
-		$data = array(
-			'id_parking' => 1,                // Assume there's a Parking record with id_Parking = 1
-			'id_place' => 1,                  // Assume there's a Place record with id_Place = 1
-			'numero_telephone' => '1234567890', // Example phone number
-			'date_heure_reservation' => '2024-06-08 14:30:00', // Example reservation timestamp
-			'duree' => 2                      // Example duration in hours
-		);
-		echo $this->Reservation_Model->insert($data);
+		$this->load->model("Utilisateur_Model");
+		var_dump($this->Utilisateur_Model->getByNamePwdType("user1@example.com","password123",1));
 	}
 }
