e67b354b235bcd0917d9979e32af6620b6c266f0
Mathieu-Safidy <148275593+Mathieu-Safidy@users.noreply.github.com>
2024-06-12 14:31:43 +0200

    Update moov-menu.js

--- front/web/assets/js/moov-menu.js
+++ front/web/assets/js/moov-menu.js
@@ -13,20 +13,13 @@ var boules = document.querySelectorAll('.moov');
 for (let index = 0; index < boules.length; index++) {
     const element = boules[index];
     element.addEventListener('click', function(){
-        if(element.querySelector('.bl-slide').classList.contains('boule')){
-            
-            element.querySelector('.bl-slide').classList.remove('boule');
-            element.querySelector('.bl-slide').classList.add('boule-2');
-        }else if(element.querySelector('.bl-slide').classList.contains('boule-2')){
-            element.querySelector('.bl-slide').classList.remove('boule-2');
-            element.querySelector('.bl-slide').classList.add('boule');
-        }
-        if(element.classList.contains('bg-danger')){
-            element.classList.remove('bg-danger');
-            element.classList.add('bg-success');
-        }else if(element.classList.contains('bg-success')){
-            element.classList.remove('bg-success');
-            element.classList.add('bg-danger');
+        element.classList.toggle('mooved');
+        if(element.classList.contains('bg-red')){
+            element.classList.remove('bg-red');
+            element.classList.add('bg-dark');
+        }else if(element.classList.contains('bg-dark')){
+            element.classList.remove('bg-dark');
+            element.classList.add('bg-red');
         }
     });
 }
