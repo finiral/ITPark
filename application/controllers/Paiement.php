2d5c61931dd453b79beee72869658f6224ca3a91
finiral <finiral@yahoo.fr>
2024-06-19 11:11:37 +0300

    added paiement

--- application/controllers/Paiement.php
+++ application/controllers/Paiement.php
@@ -4,18 +4,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Paiement extends CI_Controller
 {
     public function index($idParking){
-        $this->load->model("Parking_Model");
-        $this->load->model("Place_Model");
-        $data["nblibre"]=count($this->Place_Model->getPlaceFreeForOneParking($idParking));
-        $parking=$this->Parking_Model->getInfoParkingCompletId($idParking);
         $data["title"]="Information du parking";
         $data["description"] = "Page d'informations d'un parking";
-        $data["contents"] = "information/information";
-        $data["prix"]=$parking["prix"];
-        $data["lieu"]=$parking["lieu_nom"];
-        $data["classe"]=$parking["classe_nom"];
-        $data["debutpointe"]=$this->Parking_Model->getDebutHeurePointe(6,2023,$idParking);
-        $data["finpointe"]=$this->Parking_Model->getFinHeurePointe(6,2023,$idParking);
+        $data["contents"] = "paiement/paiement";
         $this->load->view("templates2/template2",$data);
     }
 }
