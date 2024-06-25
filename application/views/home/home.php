7757ec5e817e1463e1b09d6068905d254e990589
Mathieu-Safidy <148275593+Mathieu-Safidy@users.noreply.github.com>
2024-06-25 19:30:02 +0200

    lien for collab

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,6 +1,6 @@
 <!-- Ajouter l'élément de fond flou -->
 
-<div class="pt-5 mt-5">
+<div class="pt-5 ">
     <?php if (isset($recherche)) : ?>
     <div class="main-content min-vh-100 mt-5 p-3">
         <div class="titlePage col-7 col-lg-3 row position-absolute pub pt-2 pe-2">
@@ -16,7 +16,7 @@
 
         <div class="information  pb-3 col-lg-12">
 
-            <div class="mx-auto col-lg-7 ps-3">
+            <div class="mx-auto col-lg-7 ps-0 ps-md-3 ps-lg-3">
                 <h2 class="" style="color: rgb(0, 0, 0);">Liste des parkings</h2>
 
                 <button class="  toolbox-active btn btn-outline-danger " type="button" data-bs-toggle="collapse"
@@ -25,47 +25,48 @@
                     <span><i class="fa-solid fa-sliders"></i></span>
                     Tous les filtres
                 </button>
-            </div>
+            <!-- </div> -->
             <?php
                 if (count($recherche) == 0) { ?>
                 <div class="element col-11 col-lg-7 mt-3 mx-auto text-center py-5">
-            <h1>Aucun parking ne correspond a vos recherches</h1>
-                </div>
-            <?php } else {
-                    foreach ($recherche as $p) { ?>
-                    
-            <a href="<?php 
-                        $id=$p['id_parking'];
+                    <h1>Aucun parking ne correspond a vos recherches</h1>
+                        </div>
+                    <?php } else {
+                            foreach ($recherche as $p) { ?>
+                            
+                    <a href="<?php 
+                                $id=$p['id_parking'];
                         echo site_url("information/parking/$id");?>" style="color: black;">
-                <div class="element col-11 col-lg-7 mt-3 mx-auto">
-                    <div class="element-2">
-                        <div class="row justify-content-center">
-                            <div class="col-2">
-                                <div class="number-place text-center">
-                                    <p><?php echo $p['id_parking'] ?></p>
-                                </div>
-                            </div>
-                            <div class="col-5 ">
-                                <div class="row text-wrap">
-                                    <div class="icon-text-inline">
-                                        <i class="bi bi-geo-alt-fill"></i> <!-- Exemple d'icône Font Awesome -->
-                                        <span class="lieu"><?php echo $p['lieu_nom'] ?></span>
+                        <div class="element col-12 col-lg-121 mt-3 mx-auto">
+                            <div class="element-2">
+                                <div class="row justify-content-center">
+                                    <div class="col-2">
+                                        <div class="number-place text-center">
+                                            <p><?php echo $p['id_parking'] ?></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-5 ">
+                                        <div class="row text-wrap">
+                                            <div class="icon-text-inline">
+                                                <i class="bi bi-geo-alt-fill"></i> <!-- Exemple d'icône Font Awesome -->
+                                                <span class="lieu"><?php echo $p['lieu_nom'] ?></span>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <p>Place: <?php echo $p['nombre_place'] ?></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <h1><?php echo $p['prix'] ?> Ar</h1>
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <p>Place: <?php echo $p['nombre_place'] ?></p>
-                                </div>
-                            </div>
-                            <div class="col-4">
-                                <h1><?php echo $p['prix'] ?> Ar</h1>
                             </div>
                         </div>
-                    </div>
+                    </a>
+                    <?php }
+                        } ?>
                 </div>
-            </a>
-            <?php }
-                } ?>
-        </div>
+                    </div>
 
         <?php endif; ?>
     </div>
--- application/views/templates2/header.php
+++ application/views/templates2/header.php
@@ -2,145 +2,174 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css") ?>">
-  <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css") ?>">
-  <meta name="description" content="<?php echo $description ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="<?php echo base_url("assets/css/stylecrud.css") ?>">
-  <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css") ?>">
-  <link rel="stylesheet" href="<?php echo base_url("assets/css/all.css") ?>">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css") ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css") ?>">
+    <meta name="description" content="<?php echo $description ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/stylecrud.css") ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css") ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/all.css") ?>">
 
-  <!-- Vendor CSS Files -->
-  <link href=" <?php echo base_url("assets/css/bootstrap.min.css") ?>" rel="stylesheet">
-  <link href=" <?php echo base_url("assets/vendor/bootstrap-icons/bootstrap-icons.css") ?>" rel="stylesheet">
-  <link href=" <?php echo base_url("assets/vendor/boxicons/css/boxicons.min.css") ?>" rel="stylesheet">
-  <link href=" <?php echo base_url("assets/vendor/quill/quill.snow.css") ?>" rel="stylesheet">
-  <link href=" <?php echo base_url("assets/vendor/quill/quill.bubble.css") ?>" rel="stylesheet">
-  <link href=" <?php echo base_url("assets/vendor/remixicon/remixicon.css") ?>" rel="stylesheet">
-  <link href=" <?php echo base_url("assets/vendor/simple-datatables/style.css") ?>" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href=" <?php echo base_url("assets/css/bootstrap.min.css") ?>" rel="stylesheet">
+    <link href=" <?php echo base_url("assets/vendor/bootstrap-icons/bootstrap-icons.css") ?>" rel="stylesheet">
+    <link href=" <?php echo base_url("assets/vendor/boxicons/css/boxicons.min.css") ?>" rel="stylesheet">
+    <link href=" <?php echo base_url("assets/vendor/quill/quill.snow.css") ?>" rel="stylesheet">
+    <link href=" <?php echo base_url("assets/vendor/quill/quill.bubble.css") ?>" rel="stylesheet">
+    <link href=" <?php echo base_url("assets/vendor/remixicon/remixicon.css") ?>" rel="stylesheet">
+    <link href=" <?php echo base_url("assets/vendor/simple-datatables/style.css") ?>" rel="stylesheet">
 
 
-  <script src="<?php echo base_url("assets/js/bootstrap.min.js") ?>"></script>
-  <script src="<?php echo base_url("NiceAdmin\assets\vendor\bootstrap\js\bootstrap.bundle.min.js") ?>"></script>
-  <!-- Template Main CSS File -->
-  <link href=" <?php echo base_url("assets/css/style.css") ?>" rel="stylesheet">
-  <title><?php echo $title ?></title>
+    <script src="<?php echo base_url("assets/js/bootstrap.min.js") ?>"></script>
+    <script src="<?php echo base_url("NiceAdmin\assets\vendor\bootstrap\js\bootstrap.bundle.min.js") ?>"></script>
+    <!-- Template Main CSS File -->
+    <link href=" <?php echo base_url("assets/css/style.css") ?>" rel="stylesheet">
+    <title><?php echo $title ?></title>
 </head>
 
 <body class="bg-white ">
     <nav class="navbar navbar-expand-lg head bg-dark">
-      
+
     </nav>
 
     <!-- ======= Header ======= -->
     <header class="headerRecherche bg-dark fixed-top">
-    <div class="container-fluid justify-content-md-start justify-content-center">
-        <a class="navbar-brand brand" href="#">
+        <div class="container-fluid justify-content-md-start justify-content-center">
+            <!-- <a class="navbar-brand brand" href="#">
           <span class=""><img class="rounded" src="<?php echo base_url("assets/image/directions_car_24dp_FILL0_wght400_GRAD0_opsz24.png") ?>" alt=""></span>
           <span>
             <span class="brand-1">IT-</span><span class="brand-2">Park</span>
           </span>
-        </a>
-      </div>
-      <form method="GET" action="<?php echo site_url("accueil/recherche"); ?>">
-        <div class="justify-content-center">
-          <div class="container d-flex justify-content-center align-items-center">
-
-            <div class="col-8 col-md-6">
-              <div class="input-group">
-                <input type="text" class="form-control personalSearch" placeholder="Recherche localisation" name="localisation">
-                <span class="input-group-text" style="border-bottom-right-radius: 50px; border-top-right-radius: 50px; background-color : #DCDCDC ; color : black ;  ">
-                  <button class="rech" type="submit"><i class="bi bi-search"></i></button>
-                </span>
-              </div>
-            </div>
-          </div>
+        </a> -->
         </div>
-        
+        <form method="GET" action="<?php echo site_url("accueil/recherche"); ?>">
+            <div class="">
+                <div class="row container-fluid d-flex  align-items-center mt-3 px-0 ps-md-3">
+                    <a class="navbar-brand brand col-2 col-md-3 col-lg-4 ps-4 pe-0 me-3 me-md-0" href="#">
+                        <span class="d-none d-md-block d-lg-block jus"><img class="rounded"
+                                src="<?php echo base_url("assets/image/directions_car_24dp_FILL0_wght400_GRAD0_opsz24.png") ?>"
+                                alt="">
+                              <!-- </span> -->
+                        <!-- <span class="d-none d-md-block"> -->
+                            <span class="brand-1">IT-</span><span class="brand-2">Park</span>
+                        </span>
+                        <span class="d-block d-md-none d-lg-none">
+                            <span class="brand-1">IT-</span><span class="brand-2">P</span>
+                        </span>
+                    </a>
+                    <div class="col-7 col-md-6 col-lg-4 px-0">
+                        <div class="input-group">
+                            <input type="text" class="form-control personalSearch" placeholder="Recherche localisation"
+                                name="localisation">
+                            <span class="input-group-text"
+                                style="border-bottom-right-radius: 50px; border-top-right-radius: 50px; background-color : #DCDCDC ; color : black ;  ">
+                                <button class="rech" type="submit"><i class="bi bi-search"></i></button>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-lg-4 col-1">
+                      <div class="row justify-content-md-end ps-2">
+                        <div class="col-2 col-md-3 ps-md-5">
+                          <a href="<?php echo site_url("login/index") ?>" ><button type="button" class="btn btn-outline-secondary d-none d-md-none d-lg-block">Log-in</button></a>
+                          <a href="<?php echo site_url("login/index") ?>" ><button type="button" class="btn btn-outline-secondary d-block d-md-block d-lg-none">sign</button></a>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
 
-        <div class="mt-2 mb-2 justify-content-center ">
-          <p class="d-inline-flex gap-1">
-            <center>
-              <!-- <button class="buttonSearche  toolbox-active " type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2" style="width: 100px;">budget</button>
+
+            <!-- <div class="mt-2 mb-2 justify-content-center ">
+                <p class="d-inline-flex gap-1">
+                    <center> -->
+                        <!-- <button class="buttonSearche  toolbox-active " type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2" style="width: 100px;">budget</button>
               <button class="buttonSearche  toolbox-active " type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample3" aria-expanded="false" aria-controls="multiCollapseExample3" style="width: 100px;">classe</button>
               <button class="btn btn-primary" type="submit" data-bs-target="#multiCollapseExample3" aria-expanded="false" style="width: 100px; ">Valider</button> -->
-            </center>
-          </p>
+                    <!-- </center>
+                </p> -->
 
-          <div class="justify-content-center " id="toolbox">
-            <center>
-              <div class="row mt-2 justify-content-center">
-                <div class="col col-md-4">
-                  <div class="collapse multi-collapse" id="multiCollapseExample1" style="background-color:white;border-radius:17px">
-                    <!-- <div class="card card-body critere">
+                <div class="justify-content-center " id="toolbox">
+                    <center>
+                        <div class="row mt-2 justify-content-center">
+                            <div class="col col-md-4">
+                                <div class="collapse multi-collapse" id="multiCollapseExample1"
+                                    style="background-color:white;border-radius:17px">
+                                    <!-- <div class="card card-body critere">
                       <div class="form-check">
                         <input class="personalSearch ps-3" type="text" placeholder="rechercher" style="color:black; ">
                       </div>
                     </div> -->
-                    <div class="card card-body critere">
-                    <div class="input-group">
-                      <span class="input-group-text">min</span>
-                      <input type="text" aria-label="First name" class="form-control" name="min">
-                      <span class="input-group-text">max</span>
-                      <input type="text" aria-label="Last name" class="form-control" name="max">
-                    </div>
-                    </div>
-                    <div class="card card-body critere " style="color: black;">
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="VIP" id="flexCheckChecked" name="classe[]" checked>
-                        <label class="form-check-label" for="flexCheckChecked">
-                          Haute de gamme
-                        </label>
-                      </div>
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="Moyen" id="flexCheckChecked" name="classe[]" checked>
-                        <label class="form-check-label" for="flexCheckChecked">
-                          Moyen
-                        </label>
-                      </div>
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="Intermediaire" id="flexCheckChecked" name="classe[]" checked>
-                        <label class="form-check-label" for="flexCheckChecked">
-                          Entree de gamme
-                        </label>
-                      </div>
-                    </div>
-                    <div class="card card-body critere  ">
-                      <div class="row justify-content-end">
-                        <button class="btn btn-danger me-3" type="button" data-bs-target="#multiCollapseExample1" aria-expanded="true"  data-bs-toggle="collapse" style="width: 100px; ">Annuler</button> 
-                        <button class="btn btn-primary me-3" type="submit" data-bs-target="#multiCollapseExample1" aria-expanded="false" style="width: 100px; ">Appliquer</button> 
-                      </div>
-                    </div>
-                  </div>
-                  </div>
+                                    <div class="card card-body critere">
+                                        <div class="input-group">
+                                            <span class="input-group-text">min</span>
+                                            <input type="text" aria-label="First name" class="form-control" name="min">
+                                            <span class="input-group-text">max</span>
+                                            <input type="text" aria-label="Last name" class="form-control" name="max">
+                                        </div>
+                                    </div>
+                                    <div class="card card-body critere " style="color: black;">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="VIP"
+                                                id="flexCheckChecked" name="classe[]" checked>
+                                            <label class="form-check-label" for="flexCheckChecked">
+                                                Haute de gamme
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="Moyen"
+                                                id="flexCheckChecked" name="classe[]" checked>
+                                            <label class="form-check-label" for="flexCheckChecked">
+                                                Moyen
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="Intermediaire"
+                                                id="flexCheckChecked" name="classe[]" checked>
+                                            <label class="form-check-label" for="flexCheckChecked">
+                                                Entree de gamme
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="card card-body critere  ">
+                                        <div class="row justify-content-end">
+                                            <button class="btn btn-danger me-3" type="button"
+                                                data-bs-target="#multiCollapseExample1" aria-expanded="true"
+                                                data-bs-toggle="collapse" style="width: 100px; ">Annuler</button>
+                                            <button class="btn btn-primary me-3" type="submit"
+                                                data-bs-target="#multiCollapseExample1" aria-expanded="false"
+                                                style="width: 100px; ">Appliquer</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                 </div>
-              </div>
-            </center>
+                </center>
 
-            <div class="row mt-2 justify-content-center">
-              <div class="col col-md-4">
-                <div class="collapse multi-collapse" id="multiCollapseExample2">
-                 
+                <div class="row mt-2 justify-content-center">
+                    <div class="col col-md-4">
+                        <div class="collapse multi-collapse" id="multiCollapseExample2">
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-2 justify-content-center">
+                    <div class="col col-md-4">
+                        <div class="collapse multi-collapse" id="multiCollapseExample3">
+
+
+                        </div>
+                    </div>
+
+
 
                 </div>
-              </div>
             </div>
-            <div class="row mt-2 justify-content-center">
-              <div class="col col-md-4">
-                <div class="collapse multi-collapse" id="multiCollapseExample3">
-                  
-                
-              </div>
-            </div>
+        </form>
 
 
-
-          </div>
-        </div>
-      </form>
-
-
-      <script src="effect.js"></script>
+        <script src="effect.js"></script>
 
     </header><!-- End Header -->
